dbbcd2bacec5904beeecfb744920183238695586
Md Rashedul Alam <rashed192@gmail.com>
2017-04-19 22:10:19 +0600

    prjects.hbs added

--- server.js
+++ server.js
@@ -51,6 +51,14 @@ app.get('/',(req, res)=>{
     });
 });
 
+app.get('/projects',(req, res)=>{
+
+  res.render('projects.hbs',{
+    pageTitle:'Portfolio Page',
+    welcomeMessage : 'Some Portfolios to be added here.....'
+    });
+});
+
 app.get('/about', (req, res)=>{
   //res.send("About page");
   res.render('about.hbs',{
